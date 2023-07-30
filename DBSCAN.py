f707a47863edef247ea8a5f478c48e8fd9ed2af0
shashankmisala <110378800+shashankmisala@users.noreply.github.com>
2023-07-30 09:36:55 +0530

    Add files via upload

---
+++ DBSCAN.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import DBSCAN
+
+# Load the dataset (replace 'your_dataset.csv' with your actual dataset file)
+df = pd.read_csv('spotify_tracks.csv')
+
+# Select the relevant features for clustering
+df = df[['danceability','energy','key','loudness','mode','speechiness','acousticness','instrumentalness','liveness','valence','tempo','artists','album_name','track_name']]
+
+# Preprocess the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(df[['danceability','energy','key','loudness','mode','speechiness','acousticness','instrumentalness','liveness','valence','tempo']])
+
+# Train the DBSCAN model
+eps = 0.2 
+min_samples = 2  
+dbscan_model = DBSCAN(eps=eps, min_samples=min_samples)
+dbscan_model.fit(X_scaled)
+
+clusters = dbscan_model.labels_
+
+# Function to recommend songs
+    
+# Main Streamlit app
+def main():
+    st.title('Spotify Recommendation System With DBSCAN')
+    st.write('Input your preferences below and get song recommendations!')
+
+    danceability = st.slider('Danceability', 0, 100, 25,10)
+    energy = st.slider('Energy', 0, 100, 25,10)
+    key = st.slider('Key', 0, 100, 25,10)
+    loudness = st.slider('Loudness', 0, 100, 25,10)
+    mode = st.slider('Mode', 0, 100, 25,10)
+    speechiness = st.slider('Speechiness', 0, 100, 25,10)
+    acousticness = st.slider('Acousticness', 0, 100, 25,10)
+    instrumentalness = st.slider('Instrumentalness', 0,100,25,10)
+    liveness = st.slider('Liveness', 0, 100, 25,10)
+    valence = st.slider('Valence', 0, 100, 25,10)
+    tempo = st.slider('Tempo', 0, 100, 25,10)
+
+    if st.button('Get Recommendations'):
+    # Preprocess user data
+      user = scaler.transform([[danceability, energy, key, loudness, mode, speechiness,acousticness, instrumentalness, liveness, valence, tempo]])
+
+    # Predict the user's cluster using DBSCAN
+      user_cluster = dbscan_model.fit_predict(user)
+
+    # Filter dataset to get recommendations from the same cluster
+      recommended_songs = df[clusters == user_cluster[0]][['artists','album_name']]
+
+      st.subheader('Recommended Songs')
+      st.write(recommended_songs)
+
+if __name__ == '__main__':
+    main()
+
+
